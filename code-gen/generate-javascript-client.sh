1fa33c56752e98f8023d58784a9f2d0c4eecfa58
Justin Holmes <justinmichaelholmes@gmail.com>
2016-10-21 18:28:43 -0400

    adding code-gen

---
+++ code-gen/generate-javascript-client.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [ -e "swagger-codegen-cli.jar" ]
+then
+    echo 'swagger client is present, let us use it.'
+else
+    echo 'swagger client is not present, let us download it.'
+    wget http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar -O swagger-codegen-cli.jar
+fi
+
+rm -rf target/nodejs-server/
+java -jar swagger-codegen-cli.jar generate -l nodejs-server -i ../swagger.yaml -o target/nodejs-server/
